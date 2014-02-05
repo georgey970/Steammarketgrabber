1b9c2ad3785127412d926050339e85aafb1b9d77
Kieran Gould <kieran@kieran-stupidbox.home>
2014-02-05 22:13:53 +0000

    Added requests module into the repo

--- SteamGrabber.py
+++ SteamGrabber.py
@@ -2,3 +2,10 @@
 # Written by georgey970 and TronLaser
 
 # Uses https://github.com/smiley/steamapi
+# Uses https://pypi.python.org/pypi/requests
+
+import steamapi
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+apikey = config.get('api', 'key')
